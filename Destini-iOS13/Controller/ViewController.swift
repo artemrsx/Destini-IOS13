d59c9ffd4de04258396235f48dafc24dafa77fcf
Bochkarev Artem <bochkarevTrade@gmail.com>
2020-02-25 09:18:13 +0300

    добавил структуру

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,16 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    var activeStoryIdx = 0;
+    let story = [
+        Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+        Story(title: "You see a tiger", choice1: "Shout for help", choice2: "Play dead"),
+        Story(title: "You find a treasure chest", choice1: "Open it", choice2: "Check for traps")
+    ]
+    
+    @IBAction func choice1Action(_ sender: UIButton) {
+        if (activeStoryIdx != 0) {return};
+        activeStoryIdx = 1;
+        updateUI();
     }
-
-
+    @IBAction func choice2Action(_ sender: UIButton) {
+        if (activeStoryIdx != 0) {return};
+        activeStoryIdx = 2;
+        updateUI();
+    }
+    
+    func updateUI() {
+        storyLabel.text = story[activeStoryIdx].title;
+        choice1Button.setTitle(story[activeStoryIdx].choice1, for: .normal);
+        choice2Button.setTitle(story[activeStoryIdx].choice2, for: .normal);
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        self.updateUI();
+    }
 }
-
